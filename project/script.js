6a06f91d497098d7f144a0a8136d7eae81bac9db
Anna <anutasash@mail.ru>
2023-07-22 08:28:23 +0300

    task

--- project/script.js
+++ project/script.js
@@ -13,41 +13,41 @@ const HelloVueApp = {
                 id: 2,
                 src: 'img/articles2.png',
                 alt: 'design',
-                text: 'Kitchan Design',
-                contentText: 'Let’s Get Solution For Building ',
-                date: '26 December,2022 ',
+                text: 'Living Design',
+                contentText: 'Low Cost Latest Invented Interior Designing Ideas.',
+                date: '22 December,2022 ',
 
             }, {
                 id: 3,
                 src: 'img/articles3.png',
                 alt: 'design',
-                text: 'Kitchan Design',
-                contentText: 'Let’s Get Solution For Building ',
-                date: '26 December,2022 ',
+                text: 'Interior Design',
+                contentText: 'Best For Any Office & Business Interior    Solution',
+                date: '25 December,2022 ',
 
             }, {
                 id: 4,
                 src: 'img/articles4.png',
                 alt: 'design',
                 text: 'Kitchan Design',
-                contentText: 'Let’s Get Solution For Building ',
+                contentText: 'Best For Any Office & Business Interior  Solution ',
                 date: '26 December,2022 ',
 
             }, {
                 id: 5,
                 src: 'img/articles5.png',
                 alt: 'design',
-                text: 'Kitchan Design',
-                contentText: 'Let’s Get Solution For Building ',
-                date: '26 December,2022 ',
+                text: 'Living Design',
+                contentText: 'Low Cost Latest Invented Interior Designing Ideas.',
+                date: '22 December,2022 ',
 
             }, {
                 id: 6,
                 src: 'img/articles6.png',
                 alt: 'design',
-                text: 'Kitchan Design',
-                contentText: 'Let’s Get Solution For Building ',
-                date: '26 December,2022 ',
+                text: 'Interior Design',
+                contentText: 'Best For Any Office & Business Interior Solution ',
+                date: '25 December,2022 ',
 
             }, ]
         }
